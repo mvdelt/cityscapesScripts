7e24f029947c4f86f0e9014f1830ccd09c936c71
mvdelt <mvdelt@gmail.com>
2021-03-17 01:03:39 +0900

    j) remove Rt Lt distinction.

--- cityscapesscripts/helpers/annotation.py
+++ cityscapesscripts/helpers/annotation.py
@@ -381,7 +381,8 @@ class Annotation:
         #  Z_ORDER_J: 맨처음그릴거(맨밑바닥) -> 맨나중에그릴거(맨위) 순서.
         #  TODO: 지금 일단 걍 했는데, 연산 더 빠르게 하려면?
         # Z_ORDER_J = ['Rt_sinus', 'Lt_sinus', 'maxilla', 'mandible', 'Rt_canal', 'Lt_canal', 't_normal', 't_tx', 'impl']
-        Z_ORDER_J = ['mandible', 'Rt_sinus', 'Lt_sinus', 'maxilla', 'Rt_canal', 'Lt_canal', 't_normal', 't_tx', 'impl'] # i.21.3.7.16:48) mandible 을 젤 밑으로 내려줌.
+        # Z_ORDER_J = ['mandible', 'Rt_sinus', 'Lt_sinus', 'maxilla', 'Rt_canal', 'Lt_canal', 't_normal', 't_tx', 'impl'] # i.21.3.7.16:48) mandible 을 젤 밑으로 내려줌.
+        Z_ORDER_J = ['mandible', 'sinus', 'maxilla', 'canal', 't_normal', 't_tx', 'impl'] # i.21.3.17.1:01) Rt Lt 구분 없앤걸로 수정.
         z_ordered_objList = []
         for i in Z_ORDER_J:
             for obj in jsonDict['objects']:
